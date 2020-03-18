ebc71115baefb19e51c7a4dcfb40c14bc7a6b5a8
evgen <evgeniy.vabishevich.99@mail.ru>
2020-03-18 08:27:17 +0300

    Закинул тестовую карту в проект

---
+++ core/assets/tilesets/test_tileset/test_tileset.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="test_tileset" tilewidth="256" tileheight="256" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="90_left_road.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="straight_road.png"/>
+ </tile>
+</tileset>
